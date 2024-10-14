a6b045ab4e3f60753f60e41b58a50e350885158e
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2024-10-13 21:01:48 -0400

    Soem sample account stuff

--- app/routers/account.py
+++ app/routers/account.py
@@ -2,6 +2,22 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/account/login", tags=["account"])
+@router.post("/account/login", tags=["account"])
 async def login():
+    return {"username": "fakecurrentuser"}
+
+@router.post("/account/signup", tags=["account"])
+async def signup():
+    return {"username": "fakecurrentuser"}
+
+@router.put("/account/verify", tags=["account"])
+async def verify():
+    return {"username": "fakecurrentuser"}
+
+@router.get("/account/reset_password", tags=["account"])
+async def reset_password():
+    return {"username": "fakecurrentuser"}
+
+@router.post("/account/refresh", tags=["account"])
+async def refresh():
     return {"username": "fakecurrentuser"}
