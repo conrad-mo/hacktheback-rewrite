e05b0c330955274e8c43036558f3a1a28dfec7ea
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2024-10-16 16:13:01 -0400

    Add signup and login checks for pre-existing users

--- app/routers/account.py
+++ app/routers/account.py
@@ -10,11 +10,23 @@ router = APIRouter()
 @router.post("/login")
 async def login(user: UserLogin, session: SessionDep) -> bool:
     statement= select(Account_User).where(Account_User.email == user.email)
-    selected_user = session.exec(statement).one()
+    selected_user = session.exec(statement).first()
+    if not selected_user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail="User does not exist"
+        )
     return password_verfiy(user.password, selected_user.password)
 
 @router.post("/signup", response_model=UserPublic)
 async def signup(user: UserCreate, session: SessionDep):
+    statement= select(Account_User).where(Account_User.email == user.email)
+    selected_user = session.exec(statement).first()
+    if selected_user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail="Email already in use"
+        )
     hashed_password = hash_password(user.password)
     extra_data = {"password": hashed_password, "role": "hacker", "is_active": False}
     db_user = Account_User.model_validate(user, update=extra_data)
@@ -23,7 +35,7 @@ async def signup(user: UserCreate, session: SessionDep):
     session.refresh(db_user)
     return db_user
 
-@router.put("/verify")
+@router.put("/token")
 async def verify():
     return {"username": "fakecurrentuser"}
 
